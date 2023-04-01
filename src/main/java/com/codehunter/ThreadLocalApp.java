f5555c86adadabd94a564d6669564266f7198509
hvhai <hunterx6323@gmail.com>
2023-04-01 17:50:26 +0700

    thread local app

--- src/main/java/com/codehunter/ThreadLocalApp.java
+++ src/main/java/com/codehunter/ThreadLocalApp.java
@@ -1,13 +1,45 @@
 package com.codehunter;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * Hello world!
  *
  */
 public class ThreadLocalApp
 {
+    static final ThreadLocal<Long> CORRELATION_ID = new ThreadLocal<>();
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ThreadLocalApp app = new ThreadLocalApp();
+        app.handleRequest();
+    }
+    static long correlationId() {
+        return Math.abs(ThreadLocalRandom.current().nextLong());
+    }
+    static void initRequest() {
+        CORRELATION_ID.set(correlationId());
+    }
+
+    static void log(String message) {
+        String threadName = Thread.currentThread().getName();
+        System.out.printf("[%10s][%20s] %s%n",
+                threadName, CORRELATION_ID.get(), message);
+    }
+
+    void handleRequest() {
+        initRequest();
+        addProduct("product-1");
+        notifyShop("product-1");
+    }
+
+    void addProduct(String productName) {
+        log("Adding product: " + productName);
+        // ...
+    }
+
+    void notifyShop(String productName) {
+        log("Notifying shop about: " + productName);
+        // ...
     }
 }
