92b381b320b0bdbd103e90f4e3428cb609e885bf
hvhai <hunterx6323@gmail.com>
2023-04-01 17:37:37 +0700

    init app

---
+++ src/main/java/com/codehunter/ThreadLocalApp.java
@@ -0,0 +1,13 @@
+package com.codehunter;
+
+/**
+ * Hello world!
+ *
+ */
+public class ThreadLocalApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/codehunter/AppTest.java
@@ -0,0 +1,17 @@
+package com.codehunter;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    @Test
+    void trueTest() {
+        assertThat(Boolean.TRUE).isTrue();
+    }
+
+
+}
